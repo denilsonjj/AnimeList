b35ec40a5cd235d3b3c6608807243b3ebf7fadf5
Denilson junior <juniordenilson363@gmail.com>
2022-07-29 12:50:51 -0300

    finalizando segunda parte do projeto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,17 +4,19 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../style/Nav.module.css"
 const NavBar = () => {
     return (
         <div>
-            <Navbar bg='light' expand="lg">
+            <Navbar bg='light' text='white' expand="lg">
                 <Container>
-                    <Navbar.Brand as={Link} to="/">Anime List</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">Anime List
+                    <img src="https://img.icons8.com/color/48/000000/naruto.png"/></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            <Nav.Link as={Link} to='/Animes'>Animes</Nav.Link>
+                            <Nav.Link  style={{color:'blueviolet'}} as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link style={{color:'blueviolet'}} as={Link} to='/Animes'>Animes</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
